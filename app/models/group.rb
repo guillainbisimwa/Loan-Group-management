f78167ec11d3dd7243ac195771ff034396922e91
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-12-15 11:12:19 +0200

    Fix rubocop issues

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,3 @@
 class Group < ApplicationRecord
-
-    validates :name, presence: true, length: { maximum: 20 }
-
+  validates :name, presence: true, length: { maximum: 20 }
 end
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,6 +1,4 @@
 class Loan < ApplicationRecord
-    
-    validates :name, presence: true, length: { maximum: 20 }
-    validates :amount, presence:true, numericality: {only_float: true}
-
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :amount, presence: true, numericality: { only_float: true }
 end
