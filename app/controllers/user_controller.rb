5d2b3a45776b83f52635a827a6a76b2b08366696
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-12-16 00:36:08 +0200

    Add Sign in view

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,14 @@
 class UserController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def login
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'user/login'
+  #get 'user#login'
+  root 'user#index'
+
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
