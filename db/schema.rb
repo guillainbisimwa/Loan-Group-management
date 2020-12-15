246707ce8a1e6776fa41e17421aff43ec0897c1d
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-12-15 23:28:37 +0200

    Generate devise files for session management

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_085851) do
+ActiveRecord::Schema.define(version: 2020_12_15_211014) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "grouploans", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
-    t.integer "loan_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "loan_id", null: false
     t.index ["loan_id"], name: "index_grouploans_on_loan_id"
     t.index ["user_id"], name: "index_grouploans_on_user_id"
   end
@@ -37,7 +40,7 @@ ActiveRecord::Schema.define(version: 2020_12_15_085851) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "author_id"
-    t.integer "users_id"
+    t.bigint "users_id"
     t.index ["users_id"], name: "index_loans_on_users_id"
   end
 
@@ -45,6 +48,13 @@ ActiveRecord::Schema.define(version: 2020_12_15_085851) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "grouploans", "loans"
