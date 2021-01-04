bbb7ca69966212c87ef212a366e98f3cc146bb19
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-01-04 23:05:12 +0200

    Add User model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # # Include default devise modules. Others available are:
+  # # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+  validates :name, presence: true, length: { maximum: 20 }
+
+  has_many :loans, dependent: :destroy
+  has_many :grouploans, dependent: :destroy
+end
