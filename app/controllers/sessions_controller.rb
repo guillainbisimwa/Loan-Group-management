412ccad20a6bbb1f51020614d10cda19b794a529
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-01-06 23:04:01 +0200

    Add navigation to plash screen

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,8 +10,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session[:user_name] = params[:session][:name]
-    flash[:notice] = 'Welcome to our application'
+    @user = User.find_by(name: params[:session][:name].downcase)
+
+    redirect_to login_path, alert: 'User not found' if @user.nil?
+
+    return if @user.nil?
+
+    session[:user_id] = @user.id
+    flash[:notice] = "Welcome to our application"
     redirect_to root_path
   end
 
