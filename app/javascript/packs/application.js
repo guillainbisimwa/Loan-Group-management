f9e87c843f19a7d1b7155f72ad861f65bfec98c6
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-01-06 19:43:32 +0200

    Add navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,13 +8,28 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import "material-kit/assets/js/core/popper.min.js"
-import "material-kit/assets/js/core/bootstrap-material-design.min.js"
-import "material-kit/assets/js/material-kit.js"
+// import "material-kit/assets/js/core/popper.min.js"
+// import "material-kit/assets/js/core/bootstrap-material-design.min.js"
+// import "material-kit/assets/js/material-kit.js"
+
+
+//= require jquery2
+// import "material-kit/assets/js/core/bootstrap-material-design.min.js"
+//= require "material-kit"
+import "material-kit/assets/js/core/popper.min"
+import "material-kit/assets/js/core/bootstrap-material-design.min"
+import "material-kit/assets/js/material-kit"
 
 
 console.log("ok2")
 
+$(document).ready(function(){
+    $("button").click(function(){
+     
+console.log("ok2")
+
+    });
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
