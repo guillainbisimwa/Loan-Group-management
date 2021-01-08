9cd49d9b5541bbe67380ad296e544ed6efe084fc
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-01-09 00:24:03 +0200

    Add User reference to Group table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_085851) do
+ActiveRecord::Schema.define(version: 2021_01_08_221059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2020_12_15_085851) do
     t.datetime "createdAt"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "loans", force: :cascade do |t|
