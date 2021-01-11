c21cbdec2051a2528d6315bac35b6aa4512ccb88
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-01-11 22:57:21 +0200

    Count loans and groups and display the number in Home srceen

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,19 @@
 module UserHelper
+    def count_loans(id)
+        #loans = Loan.count(:all, :conditions => "user_id = " + id.to_s)
+        loans = Loan.where({ author_id: current_user.id }).count()
+    end
+
+    def count_external_loans()
+        n = []
+        Grouploan.all.each do |s|
+            n << s.loan_id
+        end
+        cmpt = Loan.ids.reject { |x| n.include?(x) }
+        cmpt.count
+    end
+
+    def count_groups()
+       groups = Group.count
+    end
 end
