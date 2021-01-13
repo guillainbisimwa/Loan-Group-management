ebeb2bc8ea0a85de4095152d5e38778c7d0d9502
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-01-13 13:11:18 +0200

    Confirm the owner of a group in Group model test

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Group do
   describe 'Creating an Group' do
-    let(:group) { Group.create(name: 'Barcelona') }
+
+    let(:user) { User.create(name: 'Guy') }
+
+    let(:group) { Group.create(name: 'Barcelona', user_id: user.id) }
 
     it 'confirms the name of the new group' do
       expect(group.name).to eq('Barcelona')
@@ -10,6 +13,10 @@ RSpec.describe Group do
 
     it 'fails for an invalid name for the new group' do
       expect(group.name).to_not eq('Real Madrid')
+    end
+
+    it 'confirms the owner of the new group' do
+      expect(group.user_id).to eq(user.id)
     end
 
     it 'should have many grouploans' do
