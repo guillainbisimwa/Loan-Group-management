cef59e45dedb7e3c998244758aa3172882cade1b
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-01-18 18:40:30 +0200

    Fix user-group association issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
 
+  has_many :groups
   has_many :myloans, foreign_key: :author_id, class_name: 'Loan'
 
   scope :user_all_loans, -> (current_user){ includes(:myloans).find(current_user.id) }
