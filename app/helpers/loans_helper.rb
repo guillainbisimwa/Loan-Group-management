ca872113f5fe163c884971588909915fdaa01191
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-01-20 18:09:58 +0200

    Move logic from views/loans to loans_helper

--- app/helpers/loans_helper.rb
+++ app/helpers/loans_helper.rb
@@ -18,4 +18,36 @@ module LoansHelper
   def ext_loans_sum
     Loan.external_loans_sum(current_user)
   end
+
+  def all_loans(loans)
+    loans.each do |loan|
+       concat(loans_ext(loan))
+    end
+  end
+  
+  def loans_ext(loan)
+      content_tag(:div, class: "card m-2 p-3 w-auto") do
+        concat(content_tag(:div, class: "row justify-content-arround px-3") do
+          concat(content_tag(:div, class: "mr-2") do
+            concat(icon_loan(loan))
+          end
+          )
+          concat(content_tag(:div, class: "d-flex flex-column col justify-content-around") do
+            concat(content_tag(:div, class: "row mx-1 justify-content-between") do
+              concat(content_tag :p, loan.name, class: 'font-weight-bold')
+              concat(content_tag :b, loan.amount)
+            end
+            )
+            concat(content_tag :hr, '', class: 'm-0')
+            concat(content_tag(:div, class: "row mx-1 justify-content-between") do
+              concat(content_tag :label, loan.created_at.strftime("%b %d, %Y" ).downcase, class: 'font-italic')
+              concat(content_tag :label, group_name(loan), class: 'font-italic')
+            end
+            )
+          end
+          )
+        end
+        )
+      end
+  end
 end
