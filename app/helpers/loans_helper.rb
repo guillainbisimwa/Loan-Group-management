337d863cfa038daebe1f9dd220b160805b964172
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-01-20 19:11:30 +0200

    Move logic from views/groups to groups_helper

--- app/helpers/loans_helper.rb
+++ app/helpers/loans_helper.rb
@@ -20,34 +20,26 @@ module LoansHelper
   end
 
   def all_loans(loans)
+    
     loans.each do |loan|
-       concat(loans_ext(loan))
-    end
-  end
-  
-  def loans_ext(loan)
-      content_tag(:div, class: "card m-2 p-3 w-auto") do
-        concat(content_tag(:div, class: "row justify-content-arround px-3") do
-          concat(content_tag(:div, class: "mr-2") do
+      concat(content_tag(:div, class: 'card m-2 p-3 w-auto') do
+        concat(content_tag(:div, class: 'row justify-content-arround px-3') do
+          concat(content_tag(:div, class: 'mr-2') do
             concat(icon_loan(loan))
-          end
-          )
-          concat(content_tag(:div, class: "d-flex flex-column col justify-content-around") do
-            concat(content_tag(:div, class: "row mx-1 justify-content-between") do
-              concat(content_tag :p, loan.name, class: 'font-weight-bold')
-              concat(content_tag :b, loan.amount)
-            end
-            )
-            concat(content_tag :hr, '', class: 'm-0')
-            concat(content_tag(:div, class: "row mx-1 justify-content-between") do
-              concat(content_tag :label, loan.created_at.strftime("%b %d, %Y" ).downcase, class: 'font-italic')
-              concat(content_tag :label, group_name(loan), class: 'font-italic')
-            end
-            )
-          end
-          )
-        end
-        )
-      end
+          end)
+          concat(content_tag(:div, class: 'd-flex flex-column col justify-content-around') do
+            concat(content_tag(:div, class: 'row mx-1 justify-content-between') do
+              concat(content_tag(:p, loan.name, class: 'font-weight-bold'))
+              concat(content_tag(:b, '$ ' + (loan.amount).to_s))
+            end)
+            concat(content_tag(:hr, '', class: 'm-0'))
+            concat(content_tag(:div, class: 'row mx-1 justify-content-between') do
+              concat(content_tag(:label, loan.created_at.strftime('%b %d, %Y').downcase, class: 'font-italic'))
+              concat(content_tag(:label, group_name(loan), class: 'font-italic'))
+            end)
+          end)
+        end)
+      end)
+    end
   end
 end
